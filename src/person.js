4bcf3007271a8488011b05507eb6464a4386d286
Pauolo <Pauolosavery1@gmail.com>
2022-05-23 18:19:07 +0300

    Release 2 - добавляем фильм в список

--- src/person.js
+++ src/person.js
@@ -2,12 +2,23 @@
  * Человек.
  * @param {string} name
  */
-function Person(name) {
-  // Код тут.
+ const { Cartoon, DisneyCartoon, DreamWorksCartoon } = require('../src/cartoon');
+
+function Person(name,watchedMovies) {
+  
+  this.name = name;
+  this.watchedMovies = watchedMovies;
+
+  // watchMovie: function () {
+  //   play
+  // }
 }
 
 Person.prototype.watchMovie = function (movie) {
-  // Код тут.
+  movie.play();
+  if (!this.watchedMovies.includes(movie)){
+    this.watchedMovies.push(movie)
+  }
 };
 
 module.exports = { Person };
